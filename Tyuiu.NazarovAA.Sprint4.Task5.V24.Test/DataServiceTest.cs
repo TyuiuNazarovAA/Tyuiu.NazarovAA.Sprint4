c5c464c87d17d24a2e1dc36767cbe8c704567c5f
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-16 18:55:33 +0500

    Task5

---
+++ Tyuiu.NazarovAA.Sprint4.Task5.V24.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.NazarovAA.Sprint4.Task5.V24.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint4.Task5.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[,] inputMas =
+            {
+                { 2, 2, 5, 5, -9 },
+                { 7, -3, 8, 8, 9 },
+                { 6, 7, 9, -3, 5 },
+                { -7, 5, 7, 7, 8 },
+                { 2, 2, -4, 7, 9 },
+            };
+
+            DataService ds = new DataService();
+            int res = ds.Calculate(inputMas);
+            int wait = 20;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.NazarovAA.Sprint4.Task5.V24/Program.cs
+++ Tyuiu.NazarovAA.Sprint4.Task5.V24/Program.cs
@@ -1,6 +1,6 @@
-﻿using Tyuiu.NazarovAA.Sprint4.Task4.V2.Lib;
+﻿using Tyuiu.NazarovAA.Sprint4.Task5.V24.Lib;
 
-namespace Tyuiu.NazarovAA.Sprint4.Task4.V2
+namespace Tyuiu.NazarovAA.Sprint4.Task5.V24
 {
     internal class Program
     {
@@ -12,32 +12,29 @@ namespace Tyuiu.NazarovAA.Sprint4.Task4.V2
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Тема: Двумерные массивы (статический ввод)                              *");
-            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                     *");
+            Console.WriteLine("* Задание #5                                                              *");
             Console.WriteLine("* Вариант #24                                                             *");
             Console.WriteLine("* Выполнил: Назаров Артём Александрович | СППб-25-1                       *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
-            Console.WriteLine("* статическими значениями в диапазоне от 1 до 8. Найдите минимальный      *");
-            Console.WriteLine("* элемент во втором столбце массива.                                      *");
+            Console.WriteLine("* случайными значениями в диапазоне от -7 до 6. Найти количество          *");
+            Console.WriteLine("* положительных элементов.                                                *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            Console.Write("Введите количество строк: ");
-            int rows = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите количество столбцов: ");
-            int columns = Convert.ToInt32(Console.ReadLine());
 
-            int[,] mas = new int[rows, columns]; 
+            int[,] mas = new int[5, 5];
 
-            for (int i = 0; i < rows; i++)
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < 5; j++)
                 {
-                    Console.Write($"Введите элемент [{i}, {j}]: ");
-                    mas[i, j] = Convert.ToInt32(Console.ReadLine());
+                    mas[i, j] = rnd.Next(-7, 7);
                 }
             }
 
@@ -56,17 +53,7 @@ namespace Tyuiu.NazarovAA.Sprint4.Task4.V2
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            int[,] resMas = ds.Calculate(mas);
-
-            for (int i = 0; i < resMas.GetLength(0); i++)
-            {
-                for (int j = 0; j < resMas.GetLength(1); j++)
-                {
-                    Console.Write(resMas[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
+            Console.WriteLine(ds.Calculate(mas));
         }
     }
 }
