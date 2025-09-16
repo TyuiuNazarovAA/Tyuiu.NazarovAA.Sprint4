f92f883dc3ea0b17987ad406be18e22b088855ff
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-16 18:28:04 +0500

    Task4

---
+++ Tyuiu.NazarovAA.Sprint4.Task4.V2/Program.cs
@@ -0,0 +1,72 @@
+﻿using Tyuiu.NazarovAA.Sprint4.Task4.V2.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint4.Task4.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Назаров А. А. | РППб 25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (статический ввод)                              *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Назаров Артём Александрович | СППб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 1 до 8. Найдите минимальный      *");
+            Console.WriteLine("* элемент во втором столбце массива.                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mas = new int[rows, columns]; 
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Введите элемент [{i}, {j}]: ");
+                    mas[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] resMas = ds.Calculate(mas);
+
+            for (int i = 0; i < resMas.GetLength(0); i++)
+            {
+                for (int j = 0; j < resMas.GetLength(1); j++)
+                {
+                    Console.Write(resMas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
