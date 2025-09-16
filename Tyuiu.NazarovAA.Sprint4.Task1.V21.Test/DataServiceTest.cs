025b641dd79ad7c313eff9c325a6478d0c2c21af
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-16 16:20:11 +0500

    Task1

---
+++ Tyuiu.NazarovAA.Sprint4.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NazarovAA.Sprint4.Task1.V21.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint4.Task1.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] inputMas = { 2, 4, 5, 6 };
+            int res = ds.Calculate(inputMas);
+            int wait = 48;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NazarovAA.Sprint4.Task1.V21/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.NazarovAA.Sprint4.Task1.V21.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint4.Task1.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Назаров А. А. | РППб 25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Назаров Артём Александрович | СППб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 11 элементов заполненный         *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 8 подсчитать произведение   *");
+            Console.WriteLine("* четных элементов массива.                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите длину массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] mas = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write($"Введите элемент {i}: ");
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(mas));
+        }
+    }
+}
