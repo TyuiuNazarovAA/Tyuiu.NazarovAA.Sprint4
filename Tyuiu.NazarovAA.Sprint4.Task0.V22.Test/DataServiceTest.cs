e6fc2de157bcf4093019b73a1780e094babe24b0
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-16 15:32:40 +0500

    Add project files.

---
+++ Tyuiu.NazarovAA.Sprint4.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.NazarovAA.Sprint4.Task0.V22.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint4.Task0.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] inputMas = { 2, 1, 4, 3, 8, 5, 9 };
+            int res = ds.GetSumOddArrEl(inputMas);
+            int wait = 18;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NazarovAA.Sprint4.Task0.V22/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.NazarovAA.Sprint4.Task0.V22.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint4.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Назаров А. А. | РППб 25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Назаров Артём Александрович | СППб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму нечетных *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] mas = { 2, 1, 4, 3, 8, 5, 9 };
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.GetSumOddArrEl(mas));
+        }
+    }
+}
